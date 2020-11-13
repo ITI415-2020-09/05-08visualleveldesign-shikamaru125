62139def6ea79bdb8e80bf4067a0a9c30dc17133
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-11-12 19:27:05 -0500

    PicksUps Created/ Canvas Created

---
+++ Assets/My Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void SetCountText()
+        {
+            countText.text = "Count: " + count.ToString() + "/11";
+            if(count >= 11)
+            {
+                winTextObject.SetActive(true);
+            }
+        }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+}
