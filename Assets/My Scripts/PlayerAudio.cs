1ce47cd7bc0fa687044bb2bfc344c1db9e00fae5
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-11-12 21:19:16 -0500

    Lighting/Ceilings/Canvas Finished

---
+++ Assets/My Scripts/PlayerAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+   public AudioClip splashSound;
+   public AudioSource audioS;
+
+   public AudioMixerSnapshot idleSnapshot;
+   public AudioMixerSnapshot auxInSnapshot;
+
+   private void OnTriggerEnter(Collider other)
+   {
+       if (other.CompareTag("Water"))
+       {
+           audioS.PlayOneShot(splashSound);
+       }
+       if (other.CompareTag("EndZone"))
+       {
+           auxInSnapshot.TransitionTo(0.5f);
+       }
+   }
+}
+
